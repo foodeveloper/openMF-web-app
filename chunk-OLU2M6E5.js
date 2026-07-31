14188c55779c324d98fcae915ac729b616c68403
Farah-Nahle-FOO <farah.nahle@foo.mobi>
2026-07-31 11:31:21 +0000

    Deploying to gh-pages from @ foodeveloper/openMF-web-app@2704ddd5b8e781761f6c37dbda4c579c81ac7e3c 🚀

--- chunk-OLU2M6E5.js
+++
@@ -1,1 +0,0 @@
-import{Ka as r}from"./chunk-DNFQQ6K4.js";import{ea as c,ja as e}from"./chunk-IO6GDGMU.js";var f=(()=>{class t{constructor(){this.accountingService=e(r)}resolve(){return this.accountingService.getGlAccounts()}static{this.\u0275fac=function(i){return new(i||t)}}static{this.\u0275prov=c({token:t,factory:t.\u0275fac})}}return t})();export{f as a};
